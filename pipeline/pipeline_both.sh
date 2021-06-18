12607cb009df4c146a57601e5c60380617cf510c
fjambe <81561988+fjambe@users.noreply.github.com>
2021-06-18 09:44:19 +0200

    Update pipeline_both.sh

--- pipeline/pipeline_both.sh
+++ pipeline/pipeline_both.sh
@@ -1,3 +1,3 @@
-python tree_combiner_no_weigth.py tested_MLP_R-to-L.conll tested_SVM_R-to-L.conll tested_MLP_L-to-R.conll ics_tested.conll tested_turku.conll > combined-both.conll
+python tree_combiner_no_weigth.py tested_MLP_R-to-L.conll tested_SVM_R-to-L.conll tested_MLP_L-to-R.conll ics_tested.conll tested_thom-sent_turku.conll > combined-both.conll
 
 python conllx_evaluator.py combined-both.conll Treebank_Test.conll
