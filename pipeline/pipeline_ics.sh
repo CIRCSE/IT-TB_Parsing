5772efef270368fbd90c96c980b9524a0f8b34d7
fjambe <81561988+fjambe@users.noreply.github.com>
2021-06-18 09:46:50 +0200

    Update pipeline_ics.sh

--- pipeline/pipeline_ics.sh
+++ pipeline/pipeline_ics.sh
@@ -1,3 +1,3 @@
-python tree_combiner_no_weigth.py tested_MLP_R-to-L.conll tested_SVM_R-to-L.conll tested_MLP_L-to-R.conll ics_tested.conll > combined-icspas.conll
+python tree_combiner_no_weigth.py tested_MLP_R-to-L.conll tested_SVM_R-to-L.conll tested_MLP_L-to-R.conll ics_thom-sent_tested.conll > combined-icspas.conll
 
 python conllx_evaluator.py combined-icspas.conll Treebank_Test.conll
